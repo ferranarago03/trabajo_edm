0070df9eb03801005a51ccd6084b09532de1e494
Ferran Arago <faragoausina@gmail.com>
2025-06-17 15:24:39 +0200

    Adición del valenbisi y fuentes

--- main.py
+++ main.py
@@ -3,17 +3,39 @@ from streamlit_folium import st_folium
 import folium
 import sys
 import osmnx as ox
+from shapely.geometry import shape
+import pandas as pd
 
 sys.path.append("./src/")
 
-from utils import get_route
+from utils import get_valencian_open_data, get_gdf
+
+from fuentes import get_nearest_water_fountains_on_route, print_fountains
+
+from routes import get_route, print_route, get_valenbisi_route, print_stations
+
+
+graph_cycling = ox.load_graphml("data/valencia_cycling_network.graphml")
+graph_walking = ox.load_graphml("data/valencia_walking_network.graphml")
+public_fountains = pd.read_csv("data/fonts_publiques.csv")
+public_fountains["geometry"] = public_fountains["geo_shape"].apply(
+    lambda x: shape(eval(x))
+)
+public_fountains_gdf = get_gdf(public_fountains)
+
+temp = 20
+
 
 st.title("Define inicio y fin de tu ruta")
 
 # 1. Control para elegir qué punto vamos a fijar
 st.sidebar.header("Tipo de ruta")
 # Selectbox to choose the type of route
-tipo_opciones = ["Caminando", "En bicicleta"]  # Opciones para el tipo de ruta
+tipo_opciones = [
+    "Caminando",
+    "En bicicleta",
+    "Valenbisi",
+]  # Opciones para el tipo de ruta
 type_route = st.sidebar.selectbox("¿Qué tipo de ruta quieres definir?", tipo_opciones)
 
 st.sidebar.header("Selecciona el punto de la ruta")
@@ -56,42 +78,111 @@ if st.session_state.end:
 
 # 5. Si ya tenemos ambos puntos, dibuja la ruta (línea)
 if st.session_state.start and st.session_state.end:
-    graph_cycling = ox.load_graphml("data/valencia_cycling_network.graphml")
-    graph_walking = ox.load_graphml("data/valencia_walking_network.graphml")
-
     if type_route == "Caminando":
-        route = get_route(
+        route, dist = get_route(
             (st.session_state.start["lat"], st.session_state.start["lng"]),
             (st.session_state.end["lat"], st.session_state.end["lng"]),
             graph_walking,
         )
 
-        folium.PolyLine(
-            locations=[
-                [graph_walking.nodes[node]["y"], graph_walking.nodes[node]["x"]]
-                for node in route
-            ],
-            color="blue",
-            weight=5,
-            opacity=0.7,
-        ).add_to(m)
+        print_route(route, graph_walking, m, color="blue")
+
+        fountains_on_route = get_nearest_water_fountains_on_route(
+            graph_walking,
+            dist,
+            route,
+            "Caminando",
+            temp,
+            public_fountains_gdf,
+        )
+
+        print_fountains(fountains_on_route, public_fountains_gdf, m)
 
     if type_route == "En bicicleta":
-        route = get_route(
+        route, dist = get_route(
             (st.session_state.start["lat"], st.session_state.start["lng"]),
             (st.session_state.end["lat"], st.session_state.end["lng"]),
             graph_cycling,
         )
 
-        folium.PolyLine(
-            locations=[
-                [graph_cycling.nodes[node]["y"], graph_cycling.nodes[node]["x"]]
-                for node in route
-            ],
-            color="blue",
-            weight=5,
-            opacity=0.7,
-        ).add_to(m)
+        print_route(route, graph_cycling, m, color="blue")
+
+        fountains_on_route = get_nearest_water_fountains_on_route(
+            graph_cycling,
+            dist,
+            route,
+            "En bicicleta",
+            temp,
+            public_fountains_gdf,
+        )
+
+        print_fountains(fountains_on_route, public_fountains_gdf, m)
+
+    if type_route == "Valenbisi":
+        params = {
+            "rows": 100,
+        }
+        url = "https://valencia.opendatasoft.com//api/explore/v2.1/catalog/datasets/valenbisi-disponibilitat-valenbisi-dsiponibilidad/records"
+        valenbisi_stations = get_valencian_open_data(url, params)
+
+        valenbisi_stations["geometry"] = valenbisi_stations["geo_shape"].apply(shape)
+        valenbisi_stations = get_gdf(valenbisi_stations)
+        valenbisi_stations = valenbisi_stations[valenbisi_stations["open"] == "T"]
+
+        (
+            ini_walking_route,
+            dist_ini,
+            cycling_route,
+            dist_cycling,
+            end_walking_route,
+            dist_end,
+            ini_station,
+            end_station,
+            dist,
+        ) = get_valenbisi_route(
+            (st.session_state.start["lon"], st.session_state.start["lat"]),
+            (st.session_state.end["lon"], st.session_state.end["lat"]),
+            graph_cycling,
+            graph_walking,
+            valenbisi_stations,
+        )
+
+        print_stations(ini_station, end_station, m)
+        print_route(ini_walking_route, graph_walking, m, color="green")
+        print_route(cycling_route, graph_cycling, m, color="blue")
+        print_route(end_walking_route, graph_walking, m, color="green")
+
+        fountains_ini = get_nearest_water_fountains_on_route(
+            graph_walking,
+            dist_ini,
+            route,
+            "Caminando",
+            temp,
+            public_fountains_gdf,
+        )
+
+        fountains_cycling = get_nearest_water_fountains_on_route(
+            graph_cycling,
+            dist_cycling,
+            route,
+            "En bicicleta",
+            temp,
+            public_fountains_gdf,
+        )
+
+        fountains_end = get_nearest_water_fountains_on_route(
+            graph_walking,
+            dist_end,
+            route,
+            "Caminando",
+            temp,
+            public_fountains_gdf,
+        )
+
+        print_fountains(fountains_ini, public_fountains_gdf, m)
+        print_fountains(fountains_cycling, public_fountains_gdf, m)
+        print_fountains(fountains_end, public_fountains_gdf, m)
+
 
 # 6. Plugin para capturar clics y colocar un marcador temporal
 m.add_child(
