f1eab372e37efde309a8bea07a9fc3259cd3f305
Aleixandre Tarraso Sorni <aleixandretarrasosorni@gmail.com>
2025-06-18 18:20:27 +0200

    remove od home

--- app/utils.py
+++ app/utils.py
@@ -2,8 +2,6 @@ import osmnx as ox
 import pandas as pd
 import requests
 import geopandas as gpd
-from shapely.geometry import Point
-import math
 
 
 def get_walking_network(place_name: str):
@@ -52,6 +50,7 @@ def get_valencian_open_data(url: str, params: dict = None):
 
     if response.status_code == 200:
         total_records = response.json().get("total_count")
+        print(f"Total records found: {total_records}")
 
         for start in range(0, total_records, params.get("rows", 10)):
             params["start"] = start
@@ -109,54 +108,20 @@ def get_nearest_cycle_station(graph, geovalenbisi):
     return geovalenbisi
 
 
-def get_nearest_station(point, gdf):
+def get_route(start, end, graph):
     """
-    Find the nearest station to a given point using Euclidean distance
-    after reprojecting the data to a metric CRS.
+    Get the route between two nodes in the graph.
 
     Parameters:
-        point (tuple): Tuple of coordinates (lat, lon) for the query point.
-        gdf (GeoDataFrame): GeoDataFrame of stations with geometry column in EPSG:4326.
+        start(tuple): Tuple of coordenates for the start point.
+        end: Tuple of coordinates for the end point.
+        graph: The cycling network graph.
 
     Returns:
-        GeoSeries: Row corresponding to the nearest station, including the distance in meters.
+        list: A list of nodes representing the route.
     """
+    from_node = ox.distance.nearest_nodes(graph, start[1], start[0])
+    to_node = ox.distance.nearest_nodes(graph, end[1], end[0])
 
-    gdf_proj = gdf.to_crs(epsg=25830)
-    point_geom = (
-        gpd.GeoSeries([Point(point[1], point[0])], crs="EPSG:4326")
-        .to_crs(epsg=25830)
-        .iloc[0]
-    )
-    gdf_proj["distance"] = gdf_proj.distance(point_geom)
-    nearest_row = gdf_proj.loc[gdf_proj["distance"].idxmin()]
-    return nearest_row
-
-
-def get_distance(start, end):
-    """
-    Calculate the distance between two geographic points.
-
-    Parameters:
-        start (tuple): (lat, lon) in decimal degrees.
-        end   (tuple): (lat, lon) in decimal degrees.
-
-    Returns:
-        deg_dist (float): Euclidean distance in decimal degrees.
-        gc_dist_m (float): Great-circle distance in meters.
-    """
-    lat1, lon1 = map(math.radians, start)
-    lat2, lon2 = map(math.radians, end)
-
-    # 2. Haversine (great-circle) in meters
-    dlat = lat2 - lat1
-    dlon = lon2 - lon1
-    a = (
-        math.sin(dlat / 2) ** 2
-        + math.cos(lat1) * math.cos(lat2) * math.sin(dlon / 2) ** 2
-    )
-    c = 2 * math.atan2(math.sqrt(a), math.sqrt(1 - a))
-    R = 6371000  # Earth radius in meters
-    gc_dist_m = R * c
-
-    return gc_dist_m
+    route = ox.shortest_path(graph, from_node, to_node, weight="length")
+    return route
